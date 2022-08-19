587c9c8a7a586f0689e477fa23703cfef1c03796
Akbar <akbar358888@gmail.com>
2022-08-19 08:42:38 +0500

    Initial commit

--- src/main/java/tas/uz/tasbackend/models/Model.java
+++ src/main/java/tas/uz/tasbackend/models/Model.java
@@ -13,6 +13,7 @@ public class Model {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String name;
 
     private Double price;
