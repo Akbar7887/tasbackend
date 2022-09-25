e16d537e8f82b83f05fa568df75775c04e0366e3
Akbar <akbar358888@gmail.com>
2022-09-25 13:46:17 +0500

    Initial commit

--- src/main/java/tas/uz/tasbackend/conf/CorsConfig.java
+++ src/main/java/tas/uz/tasbackend/conf/CorsConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Collections;
+import java.util.List;
 
 
 @Configuration
@@ -19,7 +20,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
 
-        config.setAllowedOrigins(Collections.singletonList("http://localhost:8087"));
+        config.setAllowedOrigins(List.of("http://localhost:8087", "https://tascom.uz"));
         config.setAllowedMethods(Collections.singletonList("GET"));
         config.setAllowedHeaders(Collections.singletonList("*"));
         source.registerCorsConfiguration("/**", config);
